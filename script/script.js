750a0c3688a9f675e46454af07bb7ccf2250b586
HankHill <hhc3h8@gmail.com>
2022-08-15 05:53:04 -0500

    committing before I probably break something

--- script/script.js
+++ script/script.js
@@ -1,40 +1,81 @@
 var MXkey = "e5af0c869b4e85411e984bc6931a21e6";
 //var musixUrl = "https://api.musixmatch.com/ws/1.1/" + MXkey;
 var YTkey  = "AIzaSyCdpNay1bVFASd8Cw1s_VRNpRNmtjsJ23E";
-
+//var ytUrl = "https://www.googleapis.com/youtube/v3/videos/search?q=" + YTkey
 var tag = document.createElement('script');
 tag.id = 'iframe-demo';
 tag.src = 'https://www.youtube.com/iframe_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+
+
+
+//var ytUrl = "https://www.googleapis.com/youtube/v3/videos/search?part=snippet&q=" +
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function searchByKeyword() {
+//   var results = Search.list('results', {q: 'music', maxResults: 10, part: 'snippet',});
+//   for(var i in results.items) {
+//     var item = results.items[i];
+//     $('#results').append().item.id.videoId, item.snippet.title;
+//   }
+// }
+
+
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     width: 640,
     height: 360,
+    videoId: "",
     playerVars: {
       'playsinline': 1,
-      'autoplay': 1,
-      'controls': 0
+      'controls': 1,
+      'showinfo': 0,
+      'showsearch': 0,
+      'enablejsapi': 1,
     },
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange,
-
     }
   });
 }
+
 function onPlayerReady(event) {
-  event.setVolume(100);
   event.target.playVideo();
 }
 
 function onPlayerStateChange(event) {
-  changeBorderColor(event.data);
+
 }
 
 function stopVideo() {
   player.stopVideo();
 }
 
+$('#searchBtn').on('click',);
+
+$(document).ready(function() {
+  $.getScript("https://www.youtube.com/iframe_api", function() {
+    onYouTubeIframeAPIReady();
+  });
+});
