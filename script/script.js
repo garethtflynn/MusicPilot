77e32256e35c96f88b3d6a1244ebe7ed46021613
HankHill <hhc3h8@gmail.com>
2022-08-16 14:04:02 -0500

    search list working; now to add functionality

--- script/script.js
+++ script/script.js
@@ -1,30 +1,20 @@
 var MXkey = "e5af0c869b4e85411e984bc6931a21e6";
 var YTkey  = "AIzaSyCdpNay1bVFASd8Cw1s_VRNpRNmtjsJ23E";
 var YTkey2 = "AIzaSyABsJT9M2cE0YeNKNhK1EVlhfYteoR5unk";
-var tag = document.createElement('script');
-tag.id = 'iframe-demo';
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
+search = "";
+var resultUrl = "./SearchResults.html";
+var player = $('#player');
+var results = $('#results');
 //  var ytUrl ="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ 
 //  search + "&key=" + {YTkey};
 
 
-var player;
+var player= $('#player');
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-    width: 640,
+    width: 480,
     height: 360,
-    videoId: "d3x4orESCnY",
-    playerVars: {
-      'playsinline': 1,
-      'controls': 1,
-      'showinfo': 0,
-      'showsearch': 0,
-      'enablejsapi': 1,
-    },
+    videoId: 'AdwkPTNnpA0',
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange,
@@ -32,11 +22,22 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 function onPlayerReady(event) {
+  player.loadVideoById();
   event.target.playVideo();
-}
+};
+
 
 function onPlayerStateChange(event) {
+
+}
+
+function playVideo (player) {
 
 }
 
@@ -45,61 +46,49 @@ function stopVideo() {
 }
 
 
-$(document).ready(function() {
-  $.getScript("https://www.youtube.com/iframe_api", function() {
-    onYouTubeIframeAPIReady();
-  });
-});
+// $(document).ready(function() {
+//   $.getScript("https://www.youtube.com/iframe_api", function() {
+//     onYouTubeIframeAPIReady();
+//   });
+// });
 
 
-function SearchHandler () {
+function SearchHandler (event) {
+  event.preventDefault();
   var search = $('#userSearch').val().trim();
   console.log(search)
-  var ytUrl ="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ 
-  search + "&key=" + YTkey2;
-  console.log(ytUrl)
+  var ytUrl="https://www.googleapis.com/youtube/v3/search?part=snippet&q="
+  +search+"&key="+YTkey;
   fetch(ytUrl)
   .then(function(response) {
     return response.json();
   })
-  .then(function(response) {
-    console.log(response)
-    for(let i = 0; i < response.items.length; i++) {
-      var titleEl = response.items[i].snippet.title;
-      //var videoId = response.items[i].Id.videoId;
-      var descr = response.items[i].snippet.description;
-      var thumbnail = response.items[i].snippet.thumbnails.default.url;
-      var channelTitle = response.items[i].snippet.channelTitle;
-      //var channelId = response.items[i].snippet.channelId;
-
-      
-
-       var resultHTML = `
-       <div>
-          <ul class "p-1 text-white">
-            <li>${titleEl}</li>
-            <li class = "#"><img src="${thumbnail}"></li>
-            <li>${descr}</li>
-            <li>${channelTitle}</li>
-          </ul>
-        </div>`;  
-    }
-    resultHTML += `</div>`;
-    $('#results').html(resultHTML);
-  })
+  .then(function(data) {
+    for (let i=0; i < data.items.length; i++) {
+      $('#results').each(function() {
+        var titleEl = data.items[i].snippet.title;
+        var VideoId = data.items[i].id.videoId;
+        var descr = data.items[i].snippet.description;
+        var thumbnail = data.items[i].snippet.thumbnails.medium.url;
+        var channelTitle = data.items[i].snippet.channelTitle;
+        //var channelId = data.items[i].snippet.channelId;
+        console.log(VideoId)
+         var resultHTML=`
+           <div class="p-2">
+            <h2 class="bg-stone-600 text-white">${titleEl}</h2>
+              <ul class="p-1 bg-slate-300 text-black">
+                 <li class = "#"><img src="${thumbnail}"></li>
+                 <span>${descr}</span>
+                 <li>${channelTitle}</li>
+              </ul>
+           </div>`;
+           resultHTML+=`</div>`;
+           $('#results').append(resultHTML)
+        })
+      }
+    })
 }
 
-$('#searchBtn').on('click', SearchHandler);
 
-
-
-      // var resultHTML = `
-      // <div class = "">
-      //   <ul class "p-1 text-white">
-      //     <li>${data.items[i].snippet.title}</li>
-      //     <li class = "#"><img src="${data.snippet[i].snippet.thumbnails.default}"></li>
-      //     <li>${data.items[i].snippet.description;}</li>
-      //     <li>${data.items[i].snippet.channelId;}</li>
-      //   </ul>
-      // </div>`;
+$('#searchBtn').on('click', SearchHandler,);
 
