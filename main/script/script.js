c4734229a1cdd85ef5eb1581dc012f6b18fe5da2
HankHill <hhc3h8@gmail.com>
2022-08-17 14:30:31 -0500

    buttonfeature

--- main/script/script.js
+++ main/script/script.js
@@ -2,19 +2,20 @@ var MXkey = "e5af0c869b4e85411e984bc6931a21e6";
 var YTkey  = "AIzaSyCdpNay1bVFASd8Cw1s_VRNpRNmtjsJ23E";
 var YTkey2 = "AIzaSyABsJT9M2cE0YeNKNhK1EVlhfYteoR5unk";
 var YTkey3 = "AIzaSyAfmhJPjuQ9Bx4x6ayWP7wbCmYlzxE6Uj8";
+//var play=$('#playBtn');
+var pause=$('#pauseBtn');
+var play=$('playBtn');
 search = "";
 VideoId="";
-var resultUrl = "./SearchResults.html";
-var player = $('#player');
-var results = $('#results');
-//  var ytUrl ="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ 
-//  search + "&key=" + {YTkey};
 
+
+//asynchronysly loads the player; don't know what asynchronus means.
 var player;
 function onYouTubeIframeAPIReady() {
   player = new window.YT.Player('player', {
-    width: 480,
-    height: 360,
+    width: 0,
+    height: 0,
+    videoId:'gwlNun99fKk',
     // loadPlaylist:{
     //   listType: "playlist",
     //   list: "",
@@ -25,68 +26,42 @@ function onYouTubeIframeAPIReady() {
     }
   });
 }
-
+//more of the asynchronus stuff the google demi-gods sent.
 var tag = document.createElement('script');
 tag.src = 'https://www.youtube.com/iframe_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
+//effects the state
 function onPlayerStateChange(event) {
 
 }
 
 
-
+//stops the player.
 function stopVideo() {
   player.stopVideo();
 }
 
-
-// $(document).ready(function() {
-//   $.getScript("https://www.youtube.com/iframe_api", function() {
-//     onYouTubeIframeAPIReady();
-//   });
-// });
-
-
+//takes the search input and plays the first result.
 function SearchHandler () {
   var search = $('#userSearch').val().trim();
   console.log(search)
   var ytUrl="https://www.googleapis.com/youtube/v3/search?part=snippet&q="
-  +search+"&key="+YTkey3;
+  +search+"&type=video"+"&videoEmbeddable=true"+"&key="+YTkey3;
   fetch(ytUrl)
   .then(function(response) {
     return response.json();
-  })
+  })//selects the top search result to shove into the player.
   .then(function(data) {
          var VideoId = data.items[0].id.videoId;
-
           console.log(VideoId);
           player.loadVideoById(VideoId);
-
   })
 }
 
-
-function onPlayerReady(event) {
-  // event.target.loadPlaylist()
+//plays the video.
+ function onPlayerReady(event) {
   event.target.playVideo();
 };
 
 $('#searchBtn').on('click', SearchHandler);
-        // var titleEl = data.items[i].snippet.title;
-        // var descr = data.items[i].snippet.description;
-        // var thumbnail = data.items[i].snippet.thumbnails.medium.url;
-        // var channelTitle = data.items[i].snippet.channelTitle;
-        //var channelId = data.items[i].snippet.channelId;
-        //  var resultHTML=`
-        //    <div class="p-2">
-        //     <h2 class="bg-stone-600 text-3xl text-white">${titleEl}</h2>
-        //       <ul class="p-1 bg-slate-300 text-black text-lg">
-        //          <li class = "#"><img src="${thumbnail}"></li>
-        //          <span>${descr}</span>
-        //          <li>${channelTitle}</li>
-        //       </ul>
-        //    </div>`;
-        //    resultHTML+=`</div>`;
-        //    $('#results').append(resultHTML)
