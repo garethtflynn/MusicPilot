dc17885b4216bc0159b84f8224c6d9dadb2439b3
HankHill <hhc3h8@gmail.com>
2022-08-17 21:03:34 -0500

    commit

--- assets/script-2.js
+++ assets/script-2.js
@@ -12,9 +12,10 @@ const saved = document.getElementById('savedData')
 var favoritesStored = JSON.parse(localStorage.getItem('savedFavorites')) || [] 
 let message = 'search by artist and song name'
 let words = message.split(' ');
+const YTkey3 = "AIzaSyAfmhJPjuQ9Bx4x6ayWP7wbCmYlzxE6Uj8";
 
 $('#searchBtn').click(function() {
-    getTrack ()
+    getTrack (), SearchHandler()
 })
 
 function displayMessage() {
@@ -114,3 +115,73 @@ function showFavorites () {
 
   }
 }
+
+//asynchronysly loads the player; don't know what asynchronus means.
+
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new window.YT.Player('player', {
+
+    width: 480,
+    height: 360,
+    videoId:'EDE-wuSTbPM',
+    playervars :{enablejsapi: 1},
+
+    width: 0,
+    height: 0,
+    videoId:'gwlNun99fKk',
+
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange,
+    }
+  });
+}
+//more of the asynchronus stuff the google demi-gods sent.
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//effects the state
+function onPlayerStateChange(event) {
+  $('#playBtn').on('click',function(){
+    if (event.data !== YT.PlayerState.PLAYING){
+      $('#playBtn').on('click',player.playVideo())
+    } else return;
+  })
+  $('#pauseBtn').on('click',function(){
+    if (event.data == YT.PlayerState.PLAYING){
+      $('#pauseBtn').on('click',player.pauseVideo())
+    } else return;
+  })
+}
+
+//stops the player I don't call this anywhere.
+//stops the player.
+function stopVideo() {
+  player.stopVideo();
+}
+
+//takes the search input and plays the first result.
+function SearchHandler () {
+  var search = $('#searchInput').val();
+  console.log(search)
+  var ytUrl="https://www.googleapis.com/youtube/v3/search?part=snippet&q="
+  +search+"&type=video"+"&videoEmbeddable=true"+"&key="+YTkey3;
+  fetch(ytUrl)
+  .then(function(response) {
+    return response.json();
+  })//selects the top search result to shove into the player.
+  .then(function(data) {
+         var VideoId = data.items[0].id.videoId;
+          console.log(VideoId);
+          player.loadVideoById(VideoId);
+  })
+}
+
+//plays the video.
+ function onPlayerReady(event) {
+  event.target.playVideo();
+};
+
